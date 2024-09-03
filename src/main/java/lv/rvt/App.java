176e77308c4bc9cc98df26877c05688a159abd72
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-09-03 09:44:51 +0000

    mainigie

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,6 +9,11 @@ public class App
     {
         
         System.out.println("Hello world");
+        //my code starts here
+        String name = "Polina";
+        int age = 17;
+        System.out.println(age);
+        
     }
 
 }
