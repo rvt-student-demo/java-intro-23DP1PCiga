31e595fa30830ec550d791d51d5831833b590f38
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-09-10 09:13:26 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,17 +2,26 @@ package lv.rvt;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args )
     {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadi savu vārdu: ");
+        String name = scanner.nextLine();
+
         
-        System.out.println("Hello world");
-        //my code starts here
-        String name = "Polina";
-        int age = 17;
-        System.out.println(age);
+        System.out.println("Ievadi savu uzvārdu: ");
+        String lastname = scanner.nextLine();
+
+        
+        System.out.println("Ievadi savu grupu: ");
+        String group = scanner.nextLine();
+
+        System.out.println("Students: " +  name  + " " + lastname  +  " , " + "grupa: " + group);
+
         
     }
 
