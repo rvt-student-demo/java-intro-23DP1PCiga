e14b8ceae6c3e0aa1e6e5a611f2ba5553513c9be
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-09-24 06:12:03 +0000

    Gift tax

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,24 +8,29 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Value of the gift? ");
+        int gift= Integer.valueOf(scanner.nextLine());
+        if( gift >5000 ) {
+        System.out.println("No tax!");
+        } else if (25000 > gift) {
+            double tax = (100 + (gift-5000)*0.08);
+            System.out.println("Tax: " + gift );
+        } else if (55000 > gift) {
+            double tax = (1700 + (gift-25000)*0.1);
+            System.out.println("Tax: " + gift );}
+          else if (200000 > gift) {
+            double tax = (4700 + (gift-55000)* 1.2);
+            System.out.println("Tax: " + gift );}
+          else if (1000000 > gift) {
+            double tax = (22100 + (gift-200000)*1.5);
+            System.out.println("Tax: " + gift );}
+          else if (gift > 1000000) ;{
+            double tax = (142100 + (gift-1000000)*1.7);
+            System.out.println("Tax: " + gift );}
 
-        System.out.println("Give the first number:");
-        int number1 = Integer.valueOf(scanner.nextLine());
 
-        System.out.println("Give the second number:");
-        int number2 = Integer.valueOf(scanner.nextLine());
-
-        if (number1 > number2){
-            System.out.println("Greater number is " + number1);}
-        else if (number1 < number2){
-            System.out.println("Greater number is " + number2);}
-        else{
-            System.out.println("The numbers are equal!");
-        }
-
-
-        }
-      }
+    }
+}
 
         
    
