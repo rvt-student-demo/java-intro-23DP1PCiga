4237cf5ecf8650a8e6c273193a9c94a14a053403
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-10-01 06:27:14 +0000

    Repeating, breaking and remembering

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,15 +7,31 @@ public class App
 {
     public static void main( String[] args )
     {
-      Scanner scanner = new Scanner(System.in);
-      int end = Integer.valueOf(scanner.nextLine());
-      int start = 0 ;
-      for (int i = end; i > 0; i--) {
-        start=start+end ;
-      }
-      System.out.println(start);
-    
-}
-}
+      Scanner reader = new Scanner(System.in);
+      int sum = 0;
+      int sk=0;
+      int even=0;
+      int odd=0;
+      System.out.print("Write numbers : ");
+      while (true) {
+          int input = Integer.valueOf(reader.nextLine());
+      
+          if (input == -1) {
+             break;
+          }
+          sum += input;
+          sk++;
+          if (input % 2 ==0){
+            even++;
+          }
+      } 
+      System.out.println("Thx! Bye!");
+      System.out.println("Sum: " + sum );
+      System.out.println("Numbers : " + sk);
+      System.out.println("Average : " + ((double) sum/sk));
+      System.out.println("Even :" + even);
+      System.out.println("Odd : " +  (sk -even));
+    }
+  }
         
    
