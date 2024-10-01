95c01dcf75d3470d7362b9211eff22f8aafb0770
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-10-01 09:10:06 +0000

    From one to parameter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,14 +8,14 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How many times ? :");
-        int input=Integer.valueOf(scanner.nextLine()) ;
-        for (int i=0; i<input ; i++){
-          printText();
-        }
+        int input=Integer.valueOf(scanner.nextLine()) ; 
     }
-    public static void printText() {
-      System.out.println("In a hole in the ground there lived a method");
+    public static void printUntilNumber(int number ) {
+      int i=0;
+      while(i>number){
+        System.out.println(i);
+        i++;
+      }
     }
   }
         
