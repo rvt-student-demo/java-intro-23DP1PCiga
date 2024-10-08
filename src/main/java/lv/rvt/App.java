d112a2405e3aa6c81cacbfd3a34c224ad65b38e7
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-10-08 07:37:18 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,19 +5,50 @@ import java.io.BufferedWriter;
 import java.util.Scanner;
 public class App 
 {
-    public static void main( String[] args )
-    {
-      divisibleByThreeInRange(3, 6);
-    }
-    int start=1;
-      public static void divisibleByThreeInRange(int start , int end){
-        while(start< end+1) {
-          if (start %3 ==0 ){
-        System.out.println(start);
+  public static void main(String[] args) {
+    printStars(5);
+    printStars(3);
+    printStars(9);
+
+    printSquare(4);
+
+    printRectangle(17, 3);
+
+    printTriangle(4);
+}
+    public static void printStars(int number){
+      for (int i=0; i<=number ; i++){
+        System.out.print("*");
       }
-      start++;
+      System.out.println();
     }
-  }
+
+
+   public static void printSquare(int size) {
+    int i =0;
+    while(i<size){
+      printStars(size);
+      i++;
+    }
+   }
+
+
+   public static void printRectangle(int width, int height){
+    int i = 0;
+    while (i<height){
+      printStars(width);
+      i++;
+    }
+   }
+
+
+public static void printTriangle(int size) {
+  int i = 0;
+  while(i < size  ){
+    printStars(i);
+    i++;
+   }
+ }
 }
 
         
