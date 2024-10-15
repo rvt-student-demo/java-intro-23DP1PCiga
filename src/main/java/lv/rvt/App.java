a2a0635ec701d763954f40e865e489646687077a
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-10-15 08:27:11 +0000

    Second plus third

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,79 +3,28 @@ package lv.rvt;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class App 
 {
-  public static void main(String[] args) {
-   
-    christmasTree(4);
-    
-}
+    public static void main(String[] args) {
+      Scanner scanner= new Scanner(System.in);
 
-    
-//star sign
-public static void printStars(int number){
-      for (int i=0; i<number ; i++){
-        System.out.print("*");
-      }
-      System.out.println();
-    }
+        ArrayList<Integer> intList = new ArrayList<>();
 
+        int input = Integer.valueOf(scanner.nextLine());
+        intList.add(input);
 
-   public static void printSquare(int size) {
-    int i =0;
-    while(i<size){
-      printStars(size);
-      i++;
-    }
-   }
+        int listSum=input;
 
-
-   public static void printRectangle(int width, int height){
-    int i = 0;
-    while (i<height){
-      printStars(width);
-      i++;
-    }
-   }
-
-
-public static void printTriangle(int size) {
-  int i = 0;
-  while(i < size  ){
-    printStars(i);
-    i++;
-   }
- }
-
-
-
-
-
-//Advanced astrology (3 parts)
- public static void printSpaces(int number){
-  for (int i=0; i<=number ; i++){
-    System.out.print(" ");
-  }
-}
- 
-public static void printTriangleTwo(int size) {
- for(int i=0; i<=size ;i++){
-  printSpaces(size-i);
-  printStars(i);
-  
-  }
- }
-
- public static void christmasTree(int height){
- for(int i=1; i<=height ;i++){
-    printSpaces(height-i);
-    printStars((i*2)-1);
-  }
-  for(int i = 0; i<2 ;i++){
-    printSpaces(height-2);
-    printStars(3);
-  }
- }
-}
+        while(input!=0){
+        input = Integer.valueOf(scanner.nextLine());
+        listSum = listSum + input ;
+        intList.add(input);
         
-   
+        }
+        System.out.println(intList.get(0));
+        System.out.println(listSum);
+        
+    }
+}
