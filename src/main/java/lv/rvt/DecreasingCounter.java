effa40d794afc2b095561f27ed07abc929b5c709
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-11-05 07:57:43 +0000

    Decreasing counter part3

--- src/main/java/lv/rvt/DecreasingCounter.java
+++ src/main/java/lv/rvt/DecreasingCounter.java
@@ -10,12 +10,13 @@ public DecreasingCounter(int initialValue){
 public void printValue() {
     System.out.println("value: "+ this.value);
 }
-
 public void decrement(){
     if(this.value !=0){
         this.value -=1;
-    
-        
     }
+}
+
+public void reset(){
+    this.value=0;
 }
 }
