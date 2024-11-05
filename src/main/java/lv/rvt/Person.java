bffd53b5c6fed17535d405143d1186ceed8c1a7c
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-11-05 10:46:07 +0000

    Statistics par1

--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -3,13 +3,17 @@ package lv.rvt;
 public class Person {
     private String name;
     private int age;
+    private int height;
+    private int weight;
 
     public Person(String name){
     this.name=name;
     this.age = 0;
+    this.height = 0;
+    this.weight = 0;
     }
-    public void printPerson() {
-        System.out.println(this.name + ", age " + this.age + " years");
+    public String toString() {
+        return this.name + ", age " + this.age + " years, my body mass index is " + this.bodyMassIndex();
  }
  
 public void growOlder() {
@@ -32,6 +36,16 @@ public String getName(){
 public void setName(String name){
     this.name = name;
 }
+public void setHeight(int newHeight) {
+    this.height = newHeight;
+}
 
+public void setWeight(int newWeight) {
+    this.weight = newWeight;
+}
+public double bodyMassIndex() {
+    double heigthPerHundred = this.height / 100.0;
+    return this.weight / (heigthPerHundred * heigthPerHundred);
+}
 
 }
---
+++ src/main/java/lv/rvt/Statistics.java
@@ -0,0 +1,19 @@
+package lv.rvt;
+
+public class Statistics {
+    private int count;
+   
+    public Statistics() {
+        this.count =count;
+        
+    }
+
+    public void addNumber(int number) {
+        
+       
+    }
+
+    public void getCount(int kopa) {
+        
+    }
+}
