1be6d2da9636820e0a349352c741b28bb40776c0
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-11-12 06:56:54 +0000

    Payment card pt2

--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -4,11 +4,19 @@ public class PaymentCard {
     private double balance;
 
 
+
 public PaymentCard(double openingBalance) {
         this.balance=openingBalance;
 }
 public String toString() {
     return "The card has a balance of "+ this.balance +" euros";
            
-  }  
+  }
+  
+public void eatAffordably() {
+    this.balance= this.balance -2.60;
+}
+public void eatHeartily() {
+    this.balance=this.balance-4.60;
+}
  }
