67167dd0a94b5b0944be6dfd016fe0e184728727
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-11-12 06:50:44 +0000

    Payment card pt1

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,28 +10,8 @@ public class App
  
   public class MainProgram {
   public static void main(String[] args) {
-  Scanner scanner = new Scanner(System.in) ;
-  int num =0;
-  
-  Statistics sum =new Statistics();
-  Statistics even =new Statistics();
-  Statistics odd =new Statistics();
-
-  while(num !=-1){
-  sum.addNumber(num) ;
-  num = Integer.valueOf(scanner.nextLine()) ;
-  if (num % 2 == 0) {
-    even.addNumber(num);
-  } 
-  if (num % 2 == 1){
-    odd.addNumber(num);
-  }
-
-  }
-  
-   System.out.println("Sum: "+ sum.sum() );
-   System.out.println(" Sum of even numbers: " + even.sum()) ;
-   System.out.println(" Sum of odd numbers: " + odd.sum());
+    PaymentCard card = new PaymentCard(50);
+    System.out.println(card);
   }
  }
 }
