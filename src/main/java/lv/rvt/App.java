7d44a7735744b745f2e52a590921446f43203113
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-12-10 09:15:38 +0000

    Card payments (part 1)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,36 +9,16 @@ import java.util.ArrayList;
 public class App 
 {
  public static void main(String[] args) throws Exception{
-    HealthStation childrensHospital = new HealthStation();
+    PaymentCard petesCard = new PaymentCard(10);
 
-    Person ethan = new Person("Ethan", 1, 110, 7);
-    Person peter = new Person("Peter", 33, 176, 85);
+        System.out.println("money " + petesCard.balance());
+        boolean wasSuccessful = petesCard.takeMoney(8);
+        System.out.println("successfully withdrew: " + wasSuccessful);
+        System.out.println("money " + petesCard.balance());
 
-    System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-    System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
-
-    childrensHospital.feed(ethan);
-    childrensHospital.feed(ethan);
-    childrensHospital.feed(ethan);
-
-    System.out.println("");
-
-    System.out.println(ethan.getName() + " weight: " + childrensHospital.weigh(ethan) + " kilos");
-    System.out.println(peter.getName() + " weight: " + childrensHospital.weigh(peter) + " kilos");
-
-    System.out.println("weighings performed: " + childrensHospital.weighings());
-
-    childrensHospital.weigh(ethan);
-    childrensHospital.weigh(peter);
-
-    System.out.println("weighings performed: " + childrensHospital.weighings());
-
-    childrensHospital.weigh(ethan);
-    childrensHospital.weigh(ethan);
-    childrensHospital.weigh(ethan);
-    childrensHospital.weigh(ethan);
-
-    System.out.println("weighings performed: " + childrensHospital.weighings());
+        wasSuccessful = petesCard.takeMoney(4);
+        System.out.println("successfully withdrew: " + wasSuccessful);
+        System.out.println("money " + petesCard.balance());
 }
 }
 
