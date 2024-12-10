e3d821e05bb63962348796c43203c8ec8f7b30bf
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-12-10 07:07:08 +0000

    PersonManager (part 3)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,37 +10,34 @@ public class App
 {
  public static void main(String[] args) throws Exception{
 Scanner scanner = new Scanner(System.in);
+Boolean isProgramRunning = true;
 
-System.out.println("Sveiki, lūdzu izvēlies komandu");
-System.out.println("show - parādīt visas personas");
-System.out.println("add - pievienot personu");
-String userCommand = scanner.nextLine();
-
-if (userCommand.equals("add")){
-
-System.out.println("Enter your name: ");
-String name = scanner.nextLine();
-
-System.out.println("Enter your age: ");
-String age = scanner.nextLine();
-
-System.out.println("Enter your weight: ");
-String weight = scanner.nextLine();
-
-System.out.println("Enter your height: ");
-String height = scanner.nextLine();
-
-Person person = new Person(name, Integer.valueOf(age) , Double.valueOf(weight) , Double.valueOf(height));
-PersonManager.addPerson(person);
-
-System.out.println("Paldies par reģistrāciju!");
-} else if(userCommand.equals("show")){
-    ArrayList<Person> persons = PersonManager.getPersonList();
-    for (Person person : persons ){
-        System.out.println(person);
+String userCommand = null;
+ while(true){
+    String command = scanner.nextLine();
+    if (command.equals("exit")){
+       System.out.println("Thanks , bye bye!");
+       isProgramRunning = false;
+    } else if (command.equals("show")){
+        ArrayList<Person> persons = PersonManager.getPersonList();
+        for (Person person : persons){
+        System.out.println(person);}
+    } else if (command.equals("help")){
+        System.out.println("Ir 4 iespējamas komandas : show , add , help un exit.");
+    }else if (command.equals("add")){
+        System.out.println("Enter your name : ");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age : ");
+        String age = scanner.nextLine();
+        System.out.println("Enter your weight: ");
+        String weight = scanner.nextLine();
+        System.out.println("Enter your height : ");
+        String height = scanner.nextLine();
+        Person person = new Person(name  ,Integer.valueOf(age) , Double.valueOf(weight), Double.valueOf(height));
+        PersonManager.addPerson(person);
     }
-   }
+    System.out.println("Your command was: " + command);
+  }
  }
 }
-
 
