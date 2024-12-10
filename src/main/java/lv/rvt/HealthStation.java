25c860b2324f9359c74b35420d52a80775cf023d
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-12-10 08:50:21 +0000

    Health station (part 2)

--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -4,4 +4,9 @@ public class HealthStation {
     public double weigh(Person person) {
         return person.getWeight();
     }
+    public void feed(Person person){
+        double weight = person.getWeight() ;
+        weight +=1;
+        person.setWeight(weight);
+    }
 }
