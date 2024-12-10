00b124ff3d7011f8a71b76ef41f07a7db9cdeb4f
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-12-10 10:30:09 +0000

    Card payments (part 2)

--- src/main/java/lv/rvt/PaymentTerminal.java
+++ src/main/java/lv/rvt/PaymentTerminal.java
@@ -1,40 +1,44 @@
 package lv.rvt;
 
 public class PaymentTerminal {
-  private double money;  
-  private int affordableMeals; 
-  private int heartyMeals;  
+    private double money;  // amount of cash
+    private int affordableMeals; // number of sold affordable meals
+    private int heartyMeals;  // number of sold hearty meals
 
-  public PaymentTerminal() {
-    double register = 1000;
-  }
-double change;
-  public double eatAffordably(double payment) {
-    double affordableMeal = 2.20;
-    if (payment < affordableMeal){
-        change = affordableMeal;
-        return change;
-    } else if (payment >= affordableMeal){
-       return double change = payment - affordableMeal;
-       return change;
-    }else{
-        return affordableMeal;}
-  }
+    public PaymentTerminal() {
+        int heartyMeals = this.heartyMeals;
+        int affordableMeals = this.affordableMeals;
 
-  public double eatHeartily(double payment) {
-    double affordableMeal = 2.20;
-    if (payment < affordableMeal){
-        change = affordableMeal;
+        double money = this.money;
+        this.money = 1000;
+    }
+    double eatingHeartily = 4.30;
+    double eatingAffordably = 2.50;
+    double change;
+
+    public double eatAffordably(double payment){
+        if (eatingAffordably <= payment){
+            this.money += payment - eatingAffordably;
+            this.affordableMeals += 1;
+            change = payment - eatingAffordably;
+        } else if (eatingAffordably > payment){
+             change = payment;
+        }
         return change;
-    } else if (payment >= affordableMeal){
-       return double change = payment - affordableMeal;
-       return change;
-    }else{
-        return affordableMeal;}
     }
 
-  public String toString() {
-      return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
-  }
-}
 
+    public double eatHeartily(double payment){
+        if (eatingHeartily <= payment){
+            this.money += payment - eatingHeartily;
+            this.heartyMeals += 1;
+            change = payment - eatingHeartily;
+        } else if (eatingHeartily > payment){
+             change = payment;
+        }
+        return change;
+    }
+    public String toString() {
+        return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+    }
+}
