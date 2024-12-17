823df337e072912ab6f5cad73780a6a2c0d7ab95
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2024-12-17 09:54:57 +0000

    Comparing apartments (2 part)

--- src/main/java/lv/rvt/Apartment.java
+++ src/main/java/lv/rvt/Apartment.java
@@ -4,15 +4,20 @@ public class Apartment {
     private int rooms;
     private int squares;
     private int pricePerSquare;
+    private int price;
+    
 
     public Apartment(int rooms, int squares, int pricePerSquare) {
         this.rooms = rooms;
         this.squares = squares;
         this.pricePerSquare = pricePerSquare;
+        this.price = price;
     }
 
-    public boolean largerThan(Apartment compared){
-       return this.squares > compared.squares;
+    public int priceDifference(Apartment compared){
+        this.price = this.squares * this.pricePerSquare;
+        int priceCompared = compared.squares * compared.pricePerSquare;
+        return(price - priceCompared);
     }
 }
 
--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
 public class App {
 public static void main(String[] args) {
     
-Apartment manhattanStudioApt = new Apartment(1, 16, 5500);
-Apartment atlantaTwoBedroomApt = new Apartment(2, 38, 4200);
-Apartment bangorThreeBedroomApt = new Apartment(3, 78, 2500);
-
-System.out.println(manhattanStudioApt.largerThan(atlantaTwoBedroomApt));       // false
-System.out.println(bangorThreeBedroomApt.largerThan(atlantaTwoBedroomApt));
+    Apartment manhattanStudioApt = new Apartment(1, 16, 5500);
+    Apartment atlantaTwoBedroomApt = new Apartment(2, 38, 4200);
+    Apartment bangorThreeBedroomApt = new Apartment(3, 78, 2500);
+    
+    System.out.println(manhattanStudioApt.priceDifference(atlantaTwoBedroomApt));  
+    System.out.println(bangorThreeBedroomApt.priceDifference(atlantaTwoBedroomApt)); 
 }
 }
 
