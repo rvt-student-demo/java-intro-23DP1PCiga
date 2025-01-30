2572b423ec034c9130251d3ef8863672ed4dba48
23DP1PCiga <166118244+23DP1PCiga@users.noreply.github.com>
2025-01-30 13:48:14 +0000

    Change History, step 2 (part 4)

--- src/main/java/lv/rvt/seondsem/ChangeHistory.java
+++ src/main/java/lv/rvt/seondsem/ChangeHistory.java
@@ -21,4 +21,34 @@ public void clear(){
 public String toString(){
     return this.records.toString();
 }
+
+public double maxValue(){
+    double max =this.records.get(0);
+    for (double value: records){
+        if (value > max){
+            max = value;
+        }
+    }
+    return max;
+}
+
+public double minValue(){
+    double min =this.records.get(0);
+    for (double value: records){
+        if (value < min){
+            min = value;
+        }
+    }
+    return min;
+}
+
+public double average(){
+    double sum = 0;
+    double cip = 0;
+    for (double value: records){
+        sum = sum +  value;
+        cip++;
+    }
+    return sum/cip;
+}
 }
